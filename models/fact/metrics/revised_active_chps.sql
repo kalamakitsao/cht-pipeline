f3f377dd33ede0718031312103b43530c173043f
Kalama Kitsao <emmkitsao@gmail.com>
2025-06-30 14:43:13 +0300

    adjusted active chp

--- models/fact/metrics/revised_active_chps.sql
+++ models/fact/metrics/revised_active_chps.sql
@@ -13,28 +13,6 @@ WITH hh_visits AS (
     GROUP BY location_id, period_id
 ),
 
-county_location AS (
-    SELECT
-        county.name AS county,
-        chp_area.location_id
-    FROM {{ ref('dim_location') }} chp_area
-    LEFT JOIN {{ ref('dim_location') }} chu ON chu.location_id = chp_area.parent_id
-    LEFT JOIN {{ ref('dim_location') }} sub ON sub.location_id = chu.parent_id
-    LEFT JOIN {{ ref('dim_location') }} county ON county.location_id = sub.parent_id
-    WHERE chp_area.level = 'chp area'
-      AND chp_area.name !~ '^[0-9]+$'
-    GROUP BY county.name, chp_area.location_id
-),
-
-expected_workload AS (
-    SELECT
-        loc.location_id,
-        e.county,
-        e.expected_households_per_month
-    FROM {{ ref('chp_expected_workload') }} e
-    JOIN county_location loc ON loc.county = e.county
-),
-
 referrals AS (
     SELECT location_id, period_id, SUM(value) AS total_referrals
     FROM {{ ref('referrals') }}
@@ -64,18 +42,25 @@ community_events AS (
     GROUP BY location_id, period_id
 ),
 
+households_registered AS (
+    SELECT location_id, period_id, SUM(value) AS households_registered
+    FROM {{ ref('households_registered') }}
+    WHERE metric_id = 'households_registered'
+    GROUP BY location_id, period_id
+),
+
 scored AS (
     SELECT
         v.location_id,
         v.period_id,
-        -- Coverage
+        -- Coverage based on households_registered
         CASE
-            WHEN v.hh_visited IS NULL OR w.expected_households_per_month IS NULL OR w.expected_households_per_month = 0 THEN 0
+            WHEN v.hh_visited IS NULL OR hr.households_registered IS NULL OR hr.households_registered = 0 THEN 0
             ELSE
                 CASE
-                    WHEN v.hh_visited::FLOAT / w.expected_households_per_month <= 0.25 THEN 10
-                    WHEN v.hh_visited::FLOAT / w.expected_households_per_month <= 0.5 THEN 20
-                    WHEN v.hh_visited::FLOAT / w.expected_households_per_month <= 0.75 THEN 30
+                    WHEN v.hh_visited::FLOAT / hr.households_registered <= 0.25 THEN 10
+                    WHEN v.hh_visited::FLOAT / hr.households_registered <= 0.5 THEN 20
+                    WHEN v.hh_visited::FLOAT / hr.households_registered <= 0.75 THEN 30
                     ELSE 40
                 END
         END AS coverage_score,
@@ -85,7 +70,7 @@ scored AS (
         CASE WHEN ce.cu_meetings >= 1 THEN 5 ELSE 0 END + CASE WHEN ce.other_events >= 1 THEN 5 ELSE 0 END AS events_score
 
     FROM hh_visits v
-    LEFT JOIN expected_workload w ON v.location_id = w.location_id
+    LEFT JOIN households_registered hr ON v.location_id = hr.location_id AND v.period_id = hr.period_id
     LEFT JOIN referrals r ON v.location_id = r.location_id AND v.period_id = r.period_id
     LEFT JOIN u5_assessed u ON v.location_id = u.location_id AND v.period_id = u.period_id
     LEFT JOIN over_5_assessed o ON v.location_id = o.location_id AND v.period_id = o.period_id
