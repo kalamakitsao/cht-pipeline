10214f0916ecacd9ab0f467ec9ea10864bbfa103
Kalama Kitsao <emmkitsao@gmail.com>
2025-07-01 11:28:54 +0300

    added locations tree

---
+++ models/fact/mv_location_hierarchy.sql
@@ -0,0 +1,33 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'chp_area_id',
+    on_schema_change = 'ignore',
+    indexes = [
+        {'columns': ['chp_area_id'], 'unique': true},
+        {'columns': ['county']},
+        {'columns': ['sub_county']},
+        {'columns': ['community_unit']},
+        {'columns': ['chp_area']}
+    ]
+) }}
+
+WITH base AS (
+    SELECT
+        county.name AS county,
+        sub.name AS sub_county,
+        chu.name AS community_unit,
+        chp_area.name AS chp_area,
+        chp_area.location_id AS chp_area_id
+    FROM {{ ref('dim_location') }} chp_area
+    LEFT JOIN {{ ref('dim_location') }} chu ON chu.location_id = chp_area.parent_id
+    LEFT JOIN {{ ref('dim_location') }} sub ON sub.location_id = chu.parent_id
+    LEFT JOIN {{ ref('dim_location') }} county ON county.location_id = sub.parent_id
+    WHERE chp_area.level = 'chp area'
+      AND chp_area.name !~ '^[0-9]+$'
+)
+
+SELECT * FROM base
+
+{% if is_incremental() %}
+WHERE chp_area_id NOT IN (SELECT chp_area_id FROM {{ this }})
+{% endif %}
