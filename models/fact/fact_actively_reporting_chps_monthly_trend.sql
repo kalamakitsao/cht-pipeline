1746203b10150b50115b658654ea0fd826e9f906
Kalama Kitsao <emmkitsao@gmail.com>
2025-09-02 09:49:16 +0300

    quick update

--- models/fact/fact_actively_reporting_chps_monthly_trend.sql
+++ models/fact/fact_actively_reporting_chps_monthly_trend.sql
@@ -8,22 +8,17 @@
 
 {% set min_ratio = var('active_min_ratio', 0.165) %}
 {% set min_abs = var('active_min_abs_visits', 15) %}
-{% set lookback_months = var('hh_lookback_months', 24) %}
 
 WITH denom AS (
   -- monthly registered households (NOT cumulative)
   SELECT location_id, period_start, value AS monthly_registered
-  FROM {{ ref('fact_households_registered_monthly_counts') }}
-  WHERE metric_id = 'households_registered_monthly'
-    AND period_start >= date_trunc('month', CURRENT_DATE) - (interval '{{ lookback_months }} months')
+  FROM {{ ref('fact_households_registered_monthly_trend') }}
 ),
 
 num AS (
   -- households visited (monthly)
   SELECT location_id, period_start, value AS monthly_visits
   FROM {{ ref('fact_households_visited_monthly_trend') }}
-  WHERE metric_id = 'hh_visited'
-    AND period_start >= date_trunc('month', CURRENT_DATE) - (interval '{{ lookback_months }} months')
 ),
 
 -- Score each CHP-month as active (1) or not (0)
