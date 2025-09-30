42d0994088acd6af00c58435a2a80dd495e7d758
kalamakitsao <133858342+kalamakitsao@users.noreply.github.com>
2025-09-30 10:41:35 +0300

    add bounds table

--- models/fact/fact_households_visited_rolling_year.sql
+++ models/fact/fact_households_visited_rolling_year.sql
@@ -19,6 +19,7 @@ base AS (
     JOIN {{ source(env_var('POSTGRES_SCHEMA'), 'household') }} h on hv.household = h.uuid
     JOIN {{ ref('mv_location_hierarchy') }} chps on hv.reported_by_parent = chps.chp_area_id
     JOIN {{ source(env_var('POSTGRES_SCHEMA'), 'contact') }} c ON hv.household = c.uuid and c.contact_type = 'e_household'
+    JOIN bounds b ON true
   WHERE hv.household IS NOT NULL 
     AND c.muted is null
     AND hv.reported::date BETWEEN b.d_start AND b.d_end
