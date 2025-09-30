50c2e26f71c6772ed36694c0ef3c2ce22342765b
kalamakitsao <133858342+kalamakitsao@users.noreply.github.com>
2025-09-30 10:25:39 +0300

    table -> model

--- models/fact/fact_households_visited_all_time.sql
+++ models/fact/fact_households_visited_all_time.sql
@@ -13,7 +13,7 @@ WITH base AS (
     hv.household          AS household_id
   FROM {{ source(env_var('POSTGRES_SCHEMA'), 'household_visit') }} hv
     JOIN {{ ref('household') }} h on hv.household = h.uuid
-    JOIN {{ source(env_var('POSTGRES_SCHEMA'), 'mv_location_hierarchy') }} chps on hv.reported_by_parent = chps.chp_area_id
+    JOIN {{ ref('mv_location_hierarchy') }} chps on hv.reported_by_parent = chps.chp_area_id
     JOIN {{ ref('contact') }} c ON hv.household = c.uuid and c.contact_type = 'e_household'
   WHERE hv.household IS NOT NULL and c.muted is null
 ),
