d7107933fd93ba79d86b7b1b1b1ac8f60b0e6eaf
kalamakitsao <133858342+kalamakitsao@users.noreply.github.com>
2025-09-30 10:27:12 +0300

    model -> household

--- models/fact/fact_households_visited_rolling_year.sql
+++ models/fact/fact_households_visited_rolling_year.sql
@@ -16,9 +16,9 @@ base AS (
     hv.reported::date     AS visit_date,
     hv.household          AS household_id
   FROM {{ source(env_var('POSTGRES_SCHEMA'), 'household_visit') }} hv
-    JOIN {{ ref('household') }} h on hv.household = h.uuid
+    JOIN {{ source(env_var('POSTGRES_SCHEMA'), 'household') }} h on hv.household = h.uuid
     JOIN {{ ref('mv_location_hierarchy') }} chps on hv.reported_by_parent = chps.chp_area_id
-    JOIN {{ ref('contact') }} c ON hv.household = c.uuid and c.contact_type = 'e_household'
+    JOIN {{ source(env_var('POSTGRES_SCHEMA'), 'contact') }} c ON hv.household = c.uuid and c.contact_type = 'e_household'
   WHERE hv.household IS NOT NULL 
     AND c.muted is null
     AND hv.reported::date BETWEEN b.d_start AND b.d_end
